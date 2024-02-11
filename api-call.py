69e60ba9b95dfbd51e08ccbccf23a1d58223ab9b
Steven Booth <sbb237@cornell.edu>
2024-02-11 01:07:39 -0500

    Test message

---
+++ api-call.py
@@ -0,0 +1,32 @@
+from http import client
+import urllib
+import json
+import os
+
+KEYS_PATH = os.path.join(os.path.dirname(__file__), "keys", "keys.json")
+with open(KEYS_PATH) as f:
+    keys = json.load(f)
+
+
+conn = client.HTTPSConnection("api.pushover.net:443")
+
+conn.request(
+    "POST",
+    "/1/messages.json",
+    urllib.parse.urlencode(
+        {
+            "token": keys["app-token"],
+            "user": keys["user-key"],
+            "message": "Time to log your mood and energy",
+            "device": keys["device-name"],
+            "url": r"https://forms.gle/ygJcKTtt4579TLoD7",
+            "url-title": "Poll Form",
+        }
+    ),
+    {"Content-type": "application/x-www-form-urlencoded"},
+)
+res = conn.getresponse()
+print(res)
+
+# TODO: If the response includes if I clicked the link, that'd be cool
+# TODO: when I eventually automate this, I need to make sure I don't emit multiple api calls at once

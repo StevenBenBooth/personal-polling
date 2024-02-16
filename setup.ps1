f05906a4638e28919dd9bb700280bf971df8d3fe
Steven Booth <sbb237@cornell.edu>
2024-02-15 19:49:32 -0500

    fix: puts WorkingDirectory argument on correct line

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,5 @@
 $taskTrigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Thursday -At 8pm
 
-$taskAction = New-ScheduledTaskAction -Execute "Powershell" -Argument "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force .venv/Scripts/Activate.ps1; python scheduler.py;"
--WorkingDirectory $PSScriptRoot
+$taskAction = New-ScheduledTaskAction -Execute "Powershell" -Argument "Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force .venv/Scripts/Activate.ps1; python scheduler.py;" -WorkingDirectory $PSScriptRoot
 
 Register-ScheduledTask 'Generate Polling Events' -Action $taskAction -Trigger $taskTrigger -TaskPath "PersonalPolling"
